d602b2f18f9f3d3320273870f2863df0b2b68095
VPAbraham <vpa4565@gmail.com>
2025-04-30 14:49:38 -0600

    Write first test to test instantiation of new ClaimsProcessor class

--- src/__tests__/claimsProcessor.test.ts
+++ src/__tests__/claimsProcessor.test.ts
@@ -1,12 +1,17 @@
-import {} from '../claimsProcessor';
+import { ClaimsProcessor, Claim } from '../claimsProcessor';
 import { testPolicies } from '../testData';
 
 describe('ClaimsProcessor', () => {
   // initialize variables
-  beforeEach('', () => {});
+  let processor: ClaimsProcessor;
+
+  beforeEach(() => {
+    // instantiate new ClaimsProcessor and pass in test policies
+    processor = new ClaimsProcessor(testPolicies);
+  });
 
   // add test blocks
-  test('', () => {
-    // expect().toBe.()
+  test('should create a claims processor with policies', () => {
+    expect(processor).toBeInstanceOf(ClaimsProcessor);
   });
 });
